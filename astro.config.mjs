1a8a2387427fa42ecf4a81ca997470c0cb0bd852
Cardenas2911 <nicolas.cardenas1129@gmail.com>
2025-12-21 04:57:40 -0500

    chore: configure netlify adapter for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import netlify from '@astrojs/netlify';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cardenasnicolas.com',
+  output: 'server',
+  adapter: netlify(),
   image: {
     domains: ['cardenasnicolas.com'],
   },
